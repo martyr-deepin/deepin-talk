1d4a389656ec259e792e3ecef86d6fefa531afa1
lovesnow <houshao55@gmail.com>
2014-01-24 16:15:48 +0800

    add dummy backend

---
+++ src/dtalk/keybinder/dummy.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2014 Deepin, Inc.
+#               2011 ~ 2014 lovesnow
+# 
+# Author:     lovesnow <houshao55@gmail.com>
+# Maintainer: lovesnow <houshao55@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from dtalk.keybinder.base import BaseBackend
+
+class DummyBackend(BaseBackend):    
+    
+    def response(self):    
+        pass
+        
+    def parse_key(self, keys):    
+        return keys
