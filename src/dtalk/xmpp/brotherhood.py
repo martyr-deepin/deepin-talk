00c7596866ecf0c59f8c52b52bf3d43628f8c548
yilang <yilang2007lw@gmail.com>
2014-02-27 13:04:14 +0800

    test brotherhood ok

--- src/dtalk/xmpp/brotherhood.py
+++ src/dtalk/xmpp/brotherhood.py
@@ -23,8 +23,7 @@ class Brotherhood(BasePlugin):
         self.xmpp.register_handler(
                 Callback("Disco Brother",
                          StanzaPath("iq/disco_brother"),
-                         self.handle_disco_users))
-
+                         self.handle_disco_brother))
         register_stanza_plugin(Iq, DiscoBrother)
         
     def plugin_end(self):
@@ -34,11 +33,13 @@ class Brotherhood(BasePlugin):
     #    BasePlugin.post_init(self)
     #    self.xmpp['xep_0030'].add_feature("deepin:iq:brotherhood")
 
-    def handle_disco_users(self, iq):
+    def handle_disco_brother(self, iq):
         if iq["type"] == "get":
-            print "handle disco users get"
+            print "handle disco brother get"
         elif iq["type"] == "result":
-            print "handle disco users result"
+            users = iq["disco_brother"]["users"]
+            print "send event for ", users
+            self.xmpp.event("get_all_users", users)
 
     def hello(self):
         print "Hello, World!"
@@ -77,54 +78,18 @@ class DiscoBrother(ElementBase):
     name = "query"
     namespace = "deepin:iq:brotherhood"
     plugin_attrib = "disco_brother"
-    interfaces = set(("method", "host"))
-    sub_interfaces = set(("host", "users"))
+    interfaces = set(("method", "host", "users"))
+    sub_interfaces = set(("host", "user"))
 
-    #_users = set()
     def setup(self, xml = None):
         ElementBase.setup(self, xml)
-        #self._users = set([user[0:2] for user in self["users"]])
 
-    #def add_user(self, jid, server = None, nickname = None):
-    #    if (jid, server) not in self._users:
-    #        self._users.add((jid, server))
-    #        user = DiscoUser(parent = self)
-    #        user["jid"] = jid
-    #        user["server"] = server
-    #        user["nickname"] = nickname
-    #        self.iterables.append(user)
-    #        return True
-    #    return False
-
-    #def del_user(self, jid, server = None):
-    #    if (jid, server) in self._users:
-    #        for user_xml in self.findall('{%s}user' % self.namespace):
-    #            user = (user_xml.attrib["jid"],
-    #                    user_xml.attrib.get("server", None)) 
-    #            if user == (jid, server):
-    #                self.xml.remove(user_xml)
-    #                return True
-    #    return False
-
-    #def get_users(self):
-    #    users = set()
-    #    for user in self["substanzas"]:
-    #        if isinstance(user, DiscoUser):
-    #            users.add(user["jid"], user["server"], user["nickname"])
-    #    return users
-
-    #def set_users(self, users):
-    #    self.del_users()
-    #    for user in users:
-    #        jid, server, nickname = user
-    #        self.add_user(jid, server, nickname)
-
-    #def del_users(self):
-    #    self._users = set()
-    #    users = [i for i in self.iterables if isinstance(i, DiscoUser)]
-    #    for user in users:
-    #        self.xml.remove(user.xml)
-    #        self.iterables.remove(user)
+    def get_users(self):
+        users = set()
+        for item in self["substanzas"]:
+            if isinstance(item, DiscoUser):
+                users.add(item.xml.attrib["jid"])
+        return users
 
 class DiscoHost(ElementBase):
     name = "host"
@@ -136,13 +101,7 @@ class DiscoUser(ElementBase):
     name = "user"
     namespace = "deepin:iq:brotherhood"
     plugin_attrib = "disco_user"
-    interfaces = set(("jid", "server", "nickname"))
-
-    def get_server(self):
-        return self._get_attr("server", None)
-
-    def get_nickname(self):
-        return self.__get_attr("nickname", None)
+    interfaces = set()
 
 register_stanza_plugin(DiscoBrother, DiscoHost, iterable = True)
 register_stanza_plugin(DiscoBrother, DiscoUser, iterable = True)
